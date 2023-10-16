4afc4c4fb61d67f3d7b565a2eb95901f57085dc7
Ian Skelskey <ianskelskey@gmail.com>
2023-10-15 20:41:37 -0400

    Upload database design work

---
+++ sql/problem_7-6_query.sql
@@ -0,0 +1,6 @@
+-- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
+SELECT EMPLOYEE.EMP_NUM, EMP_LNAME, ROUND(SUM(ASSIGN_HOURS), 1) AS SumOfASSIGN_HOURS, ROUND(SUM(ASSIGN_CHARGE), 2) AS SumOfASSIGN_CHARGE
+FROM ASSIGNMENT
+INNER JOIN EMPLOYEE 
+ON ASSIGNMENT.EMP_NUM = EMPLOYEE.EMP_NUM
+GROUP BY EMP_NUM
---
+++ sql/problem_7-9_query.sql
@@ -0,0 +1,3 @@
+-- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
+SELECT COUNT(*)
+FROM INVOICE
